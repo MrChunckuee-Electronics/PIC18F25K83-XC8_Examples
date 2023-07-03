3a9fce6622d94095ee2d1dfa86d54ae4a0e42366
Pedro Sánchez <mrchunckuee.psr@gmail.com>
2023-07-02 23:01:14 -0600

    Lab002: Atender el desbordamiento del Timer0 con interrupciones

---
+++ XC8-18F25K83_Lab002_TMR0_Interrupt.X/build/default/production/main.p1.d
@@ -0,0 +1,6 @@
+build/default/production/main.p1:  \
+main.c  \
+device_config.h  \
+main.h  \
+mcu.h  \
+isr_manager.h 
---
+++ XC8-18F25K83_Lab002_TMR0_Interrupt.X/build/default/production/mcu.p1.d
@@ -0,0 +1,5 @@
+build/default/production/mcu.p1:  \
+mcu.c  \
+main.h  \
+mcu.h  \
+isr_manager.h 
